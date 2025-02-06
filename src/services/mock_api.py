f50bc11c870b0515f6ff39feb3705e6dba83b8c5
Sergio S <TheLastRkoch@gmail.com>
2025-02-06 13:20:40 -0600

    Fix a bug related with multiple endpoints

--- src/services/mock_api.py
+++ src/services/mock_api.py
@@ -3,6 +3,7 @@ from flask import Flask
 import json
 
 from utils.file import UtilFile
+from utils.prompt import UtilPrompt
 
 
 class ServiceMockAPI:
@@ -13,25 +14,27 @@ class ServiceMockAPI:
 
         # Utils definition
         util_file = UtilFile()
+        prompt = UtilPrompt()
 
         route_list_path = env["ROUTE_LIST_PATH"]
+        util_file.open(route_list_path)
+        prompt.wait("Please configure the endpoints then press any key")
         route_list = json.loads(util_file.read_text_file(route_list_path))
 
         if route_list is None or len(route_list) > 0:
             app = Flask(__name__)
 
             for route in route_list:
+                name = route.get("name")
                 path = route.get("path")
                 response = route.get("response")
                 status_code = route.get("status_code")
-                app.add_url_rule(path, view_func=lambda: self.handler(response, status_code))
-                print(f"{path} configured")
 
+                app.add_url_rule(rule=path,
+                                 view_func=lambda response=response, status_code=status_code: self.handler(
+                                     response, status_code),
+                                 endpoint=name)
             app.run(debug=True)
+            print("Mock API running")
         else:
             print("Error: Could not load the configured endpoints.")
-
-
-if __name__ == "__main__":
-    service_mock_api = ServiceMockAPI()
-    service_mock_api.run()
