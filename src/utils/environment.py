b113f4a65d687d60c492e127385dde7f3d429a05
Sergio S <thelastrkoch@gmail.com>
2025-03-24 19:36:35 +0000

    Fixing issues

--- src/utils/environment.py
+++ src/utils/environment.py
@@ -8,7 +8,7 @@ class UtilEnvironment:
         load_dotenv()
         variable_dict = {}
         for variable_name in variable_list:
-            if variable_content := env.get(variable_name) is None:
+            if env.get(variable_name) is None:
                 raise ValueError(f"The env variable {variable_name} could not be empty")
-            variable_dict[variable_name] = variable_content
+            variable_dict[variable_name] = env.get(variable_name)
         return variable_dict
