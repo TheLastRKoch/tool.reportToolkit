770eb96b1d8956c0d40e2e9d95302b3a5fa958d0
Sergio S <thelastrkoch@gmail.com>
2025-03-24 19:30:15 +0000

    Resolve issue related to the get_and_check_variables method

--- src/utils/environment.py
+++ src/utils/environment.py
@@ -1,11 +1,14 @@
+from dotenv import load_dotenv
 from os import environ as env
 
 
-@classmethod
-def get_and_check_variables(variable_list):
-    variable_dict = {}
-    for variable_name in variable_list:
-        if variable_content := env.get(variable_name) is None:
-            raise ValueError(f"The env variable {variable_name} could not be empty")
-        variable_list[variable_name] = variable_content
-    return variable_dict
+class UtilEnvironment:
+    @classmethod
+    def get_and_check_variables(self, variable_list):
+        load_dotenv()
+        variable_dict = {}
+        for variable_name in variable_list:
+            if variable_content := env.get(variable_name) is None:
+                raise ValueError(f"The env variable {variable_name} could not be empty")
+            variable_dict[variable_name] = variable_content
+        return variable_dict
